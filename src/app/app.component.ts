e77c2d13021340ba78645798d91b2088d1574f89
zeljkosamed <smdkthyli@gmail.com>
2022-02-13 18:08:24 +0300

    Proje oluşturuldu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-generator';
+
+  password = '';
+  length:number = 0;
+  includeLetters:boolean = false;
+  includeNumbers:boolean = false;
+  includeSymbols:boolean = false;
+
+  modifyLength(value:string) {
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+
+  modifyLetters() {
+    this.includeLetters = !this.includeLetters;  
+  }
+
+  modifyNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  buttonClick() {
+    const numbers = '123456789';
+    const letters = 'abcdefghijklmnoprstuvwyz';
+    const symbols = '!@#$+-*/';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random()*validChars.length);
+     generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+  }
+
 }
